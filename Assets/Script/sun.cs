84913702ad96ba43d96a0d00f70e7986df71ab33
ZodicLight <40487226+ZodicLight@users.noreply.github.com>
2022-05-11 14:57:02 +1000

    FirstCommit

---
+++ Assets/Script/sun.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sun : MonoBehaviour {
+//function: Day night circle 
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround (Vector3.zero, Vector3.right, 11f * Time.deltaTime);//Rotate two light source. 
+		//transform.LookAt (Vector3.zero);
+	}
+}
