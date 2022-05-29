a16338db9d5e81b928e6b3f57b9e9399bb20acd7
ZodicLight <40487226+ZodicLight@users.noreply.github.com>
2022-05-30 03:33:32 +1000

    Sun Movement Script

---
+++ Assets/Kevin/SunMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunMovement : MonoBehaviour
+{
+
+    public Transform target; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = target.position; 
+    }
+}
